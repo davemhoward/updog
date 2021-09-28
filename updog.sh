64f6a9b3f46f3c149ef28649c446604fa4d2ba5a
HOWARD David <dhoward2@login01.ecdf.ed.ac.uk>
2021-09-28 18:28:06 +0100

    launcher working, sh script working, R script input ok - but rest to update

--- updog.sh
+++ updog.sh
@@ -1,40 +1,71 @@
 #!/bin/bash
 
-#$ -cwd
-#$ -l h_vmem=16G
-# -t 22-22
-
-##SGE_TASK_ID=1
-##SGE_TASK_ID=$1
+# 1 chr
+# 2 $testloc
+# 3 $testtype
+# 4 $ldloc
+# 5 $ldtype
+# 6 $sumstats
+# 7 $scores
+# 8 $plinkloc
+# 9 $rloc
+# 10 $outname
+SGE_TASK_ID=1
 
 . /etc/profile.d/modules.sh
-module load igmm/apps/R/4.0.2
-module load igmm/apps/plink/2.03
+module load "$8"
+module load "$9"
 
-plink2 \
---bfile ldref/g1000_eur_chr$1 \
---chr $1 \
---from-bp $(head -n1 chunkscores_chr$1_$(printf "%03d" ${SGE_TASK_ID}) | awk '{print $3-250000}') \
---to-bp $(tail -n1 chunkscores_chr$1_$(printf "%03d" ${SGE_TASK_ID}) | awk '{print $3+250000}') \
---make-bed \
---out ~/scratch/g1000_eur_chr$1_$(printf "%03d" ${SGE_TASK_ID})
+start=$(head -n1 temp/chunkscores_${10}_chr"$1"_$(printf "%03d" ${SGE_TASK_ID}) | awk '{print $2-250000}')
+stop=$(tail -n1 temp/chunkscores_${10}_chr"$1"_$(printf "%03d" ${SGE_TASK_ID}) | awk '{print $2+250000}')
 
-plink2 \
---pfile ukb/ukb_chr$1 \
---chr $1 \
---from-bp $(head -n1 chunkscores_chr$1_$(printf "%03d" ${SGE_TASK_ID}) | awk '{print $3-250000}') \
---to-bp $(tail -n1 chunkscores_chr$1_$(printf "%03d" ${SGE_TASK_ID}) | awk '{print $3+250000}') \
---exclude ukb/duplicatedSNPs_chr$1.txt \
---make-bed \
---out ukb/ukb_chr$1_$(printf "%03d" ${SGE_TASK_ID})
+echo ""
+if grep -q "plink/2" <<< "$8"; then ## chunk ldref and test date using plink 2
+  echo "  Chunking ld reference data"
+  echo ""
+  plink2 \
+  --${5} ${4}${1} \
+  --chr $1 \
+  --from-bp $start \
+  --to-bp $stop \
+  --make-bed \
+  --out temp/ldref_${10}_chr${1}_$(printf "%03d" ${SGE_TASK_ID})
+  echo ""
+  echo "  Chunking test data"
+  echo ""
+  plink2 \
+  --${3} ${2}${1} \
+  --chr $1 \
+  --from-bp $start \
+  --to-bp $stop \
+  --make-bed \
+  --out temp/testdata_${10}_chr${1}_$(printf "%03d" ${SGE_TASK_ID})
+else ## chunk ldref and test date using plink
+  echo "  Chunking ld reference data"
+  echo ""
+  plink \
+  --${5} ${4}${1} \
+  --chr $1 \
+  --from-bp $start \
+  --to-bp $stop \
+  --make-bed \
+  --out temp/ldref_${10}_chr${1}_$(printf "%03d" ${SGE_TASK_ID})
+  echo ""
+  echo "  Chunking test data"
+  echo ""
+  plink \
+  --${3} ${2}${1} \
+  --chr $1 \
+  --from-bp $start \
+  --to-bp $stop \
+  --make-bed \
+  --out temp/testdata_${10}_chr${1}_$(printf "%03d" ${SGE_TASK_ID})
+fi
+echo ""
 
-head -n 1 23andmePGC_sumstats.ma > 23andmePGC_sumstats_chr$1_$(printf "%03d" ${SGE_TASK_ID}).ma
+## chunk summary stats that are in testdata
 awk 'NR==FNR {FILE1[$2]=$0; next} ($1 in FILE1) {print $0}' \
-ukb/ukb_chr$1_$(printf "%03d" ${SGE_TASK_ID}).bim 23andmePGC_sumstats.ma >> 23andmePGC_sumstats_chr$1_$(printf "%03d" ${SGE_TASK_ID}).ma
+temp/testdata_${10}_chr${1}_$(printf "%03d" ${SGE_TASK_ID}).bim ${6} > temp/sumstats_${10}_chr$1_$(printf "%03d" ${SGE_TASK_ID}).txt
 
-## chromsome as $1 and task id as chunk
-Rscript updog3_for_eddie.R $1 ${SGE_TASK_ID}
-
-rm ~/scratch/g1000_eur_chr$1_$(printf "%03d" ${SGE_TASK_ID})*
-rm ukb/ukb_chr$1_$(printf "%03d" ${SGE_TASK_ID})*
-rm 23andmePGC_sumstats_chr$1_$(printf "%03d" ${SGE_TASK_ID}).ma
+## submit chunk to updog.R
+#Rscript updog.R $1 ${SGE_TASK_ID} $10
