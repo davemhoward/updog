e788eee335d3f3dd59c3445754175cc8e8c26830
David Howard <k1812035@login3.pri.rosalind2.alces.network>
2022-05-04 18:34:45 +0100

    continue to move to slurm

--- merge_chunks.sbatch.sh
+++ merge_chunks.sbatch.sh
@@ -3,21 +3,22 @@
 # supplied arguments: $1=$testloc, $2=$testtype, $3=$ldloc, $4=$ldtype
 # $5=$sumstats, $6=$scores, $7=$plinkloc, $8=$rloc, $9=$outname, $10=sumstatsOR
 
-module load "$8"
+module load "$rloc"
 
 ## pass to merge_chunks.R
-Rscript merge_chunks.R ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10}
+Rscript merge_chunks.R ${testloc} ${testtype} ${ldloc} ${ldtype} ${sumstats} ${scores} ${plinkloc} ${rloc} ${outname} ${sumstatsOR}
 
 ## if resubmitjobs file created then update to allow execute
-if [ -e $(eval echo  resubmitjobs_${9}) ]; then
-  chmod 700 resubmitjobs_${9}
+if [ -e $(eval echo  resubmitjobs_${outname})* ]; then
+  chmod 700 resubmitjobs_${outname}*
 fi
 
 ## if genomewide scores created then remove chunked files
-if [ -e $(eval echo genomewidescores_${9}.txt) ]; then
-  rm temp/chunkscores_${9}_chr*
-  rm temp/ldref_${9}_chr*
-  rm temp/scoreoutput_${9}_chr*
-  rm temp/sumstats_${9}_chr*
-  rm temp/testdata_${9}_chr*
+if [ -e $(eval echo genomewidescores_${outname}.txt) ]; then
+  rm temp/chunkscores_${outname}_chr*
+  rm temp/ldref_${outname}_chr*
+  rm temp/scoreoutput_${outname}_chr*
+  rm temp/sumstats_${outname}_chr*
+  rm temp/testdata_${outname}_chr*
 fi
+
